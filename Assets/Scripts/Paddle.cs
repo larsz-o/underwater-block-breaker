5bb318b549ba8f3ee4cc7bd4ed92a11bb960ca29
Lars <larsmackenzie@gmail.com>
2020-01-23 15:46:18 -0500

    paddle moves along with mouse

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -14,6 +14,13 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition.x/Screen.width * screenWidthInUnits);
+        //gets the position of the mouse's x axis and divides it by the screen width, then converts that number into unity world units
+        float mousePosInUnits = Input.mousePosition.x/Screen.width * screenWidthInUnits;
+        // creates a new 2D vector (which is attached to our paddle object)
+        Vector2 paddlePos = new Vector2 (transform.position.x, transform.position.y);
+        // set a limit on where the vector can move (avoid it from going off screen)
+        // min value is 1 (the screen width divided by itself = 1, and the max is the screen width minus 1 since the paddle has a width as well)
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, screenWidthInUnits/screenWidthInUnits, screenWidthInUnits-1);
+        transform.position = paddlePos;
     }
 }
