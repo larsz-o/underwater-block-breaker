d0e489792e5ca502bfd8ea9f6e8a8c8405297935
Lars <larsmackenzie@gmail.com>
2020-01-25 14:05:31 -0500

    create method to count how many blocks are in a level

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+    Level level; // cached reference 
+
+    private void Start ()
+    {
+        level = FindObjectOfType<Level>();
+        // each block will "count" itself at the start of play
+        level.CountBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z));
         Destroy(gameObject);
     }
 }
